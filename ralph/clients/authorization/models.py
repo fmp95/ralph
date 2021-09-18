57e184193131d864b418407c1fdae2c874a77f8e
Frederico Plotze <plotze.frederico.social@gmail.com>
2021-09-18 09:14:42 -0300

    Change model imports

--- ralph/clients/authorization/models.py
+++ ralph/clients/authorization/models.py
@@ -1,10 +1,17 @@
 import uuid
 
-from django.db import models
+from django.db.models import (
+    AutoField,
+    BooleanField,
+    CharField,
+    DateTimeField,
+    ManyToManyField,
+    Model,
+)
 from django.utils.timezone import now
 
 
-class User(models.Model):
+class User(Model):
 
     """
     Model for user entries.
@@ -12,41 +19,41 @@ class User(models.Model):
     This class models database table to store user information.
 
     Attributes:
-        user_uuid (object): VARCHAR column to store user unique identifier.
-        username (object): VARCHAR column to store user username.
-        password (object): VARCHAR column to store user password.
-        email (object): VARCHAR column to store user email address.
-        first_name (object): VARCHAR column to store user first name.
-        last_name (object): VARCHAR column to store user last name.
-        is_active (object): BOOLEAN column to store information if user is active.
-        created (object): DATETIME column to store date of user creation.
-        updated (object): DATETIME column to store date of user last updated.
-        roles (object): MANYTOMANY relation between user table and role table.
+        user_uuid (CharField): VARCHAR column to store user unique identifier.
+        username (CharField): VARCHAR column to store user username.
+        password (CharField): VARCHAR column to store user password.
+        email (CharField): VARCHAR column to store user email address.
+        first_name (CharField): VARCHAR column to store user first name.
+        last_name (CharField): VARCHAR column to store user last name.
+        is_active (BooleanField): BOOLEAN column to store information if user is active.
+        created (DateTimeField): DATETIME column to store date of user creation.
+        updated (DateTimeField): DATETIME column to store date of user last updated.
+        roles (ManyToManyField): MANYTOMANY relation between user table and role table.
     """
 
-    user_uuid = models.CharField(
+    user_uuid = CharField(
         max_length=40,
         primary_key=True,
         default=uuid.uuid4,
         null=False,
         db_column="uuid",
     )
-    username = models.CharField(max_length=30, null=False, unique=True)
-    password = models.CharField(max_length=100, null=False)
-    email = models.CharField(max_length=100, null=False)
-    first_name = models.CharField(max_length=50, null=False)
-    last_name = models.CharField(max_length=50, null=False)
-    is_active = models.BooleanField(default=False, null=False)
-    created = models.DateTimeField(default=now)
-    updated = models.DateTimeField(default=now)
+    username = CharField(max_length=30, null=False, unique=True)
+    password = CharField(max_length=100, null=False)
+    email = CharField(max_length=100, null=False)
+    first_name = CharField(max_length=50, null=False)
+    last_name = CharField(max_length=50, null=False)
+    is_active = BooleanField(default=False, null=False)
+    created = DateTimeField(default=now)
+    updated = DateTimeField(default=now)
 
-    roles = models.ManyToManyField(to="Role")
+    roles = ManyToManyField(to="Role")
 
     class Meta:
         db_table = "users"
 
 
-class Role(models.Model):
+class Role(Model):
 
     """
     Model for user roles entries.
@@ -54,28 +61,28 @@ class Role(models.Model):
     This class models database table to store user roles information.
 
     Attributes:
-        id (object): INTEGER column with auto increment used as role id.
-        name (object): VARCHAR column to store name of role.
-        description (object): VARCHAR column to store basic description of the role.
-        created (object): DATETIME column to store date of role creation.
-        updated (object): DATETIME column to store date of role last updated.
-        permissions (object): MANYTOMANY relation between role table and permission
-            table.
+        id (AutoField): INTEGER column with auto increment used as role id.
+        name (CharField): VARCHAR column to store name of role.
+        description (CharField): VARCHAR column to store basic description of the role.
+        created (DateTimeField): DATETIME column to store date of role creation.
+        updated (DateTimeField): DATETIME column to store date of role last updated.
+        permissions (ManyToManyField): MANYTOMANY relation between role table and
+            permission table.
     """
 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, unique=True, null=False)
-    description = models.CharField(max_length=150, null=True)
-    created = models.DateTimeField(default=now)
-    updated = models.DateTimeField(default=now)
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=50, unique=True, null=False)
+    description = CharField(max_length=150, null=True)
+    created = DateTimeField(default=now)
+    updated = DateTimeField(default=now)
 
-    permissions = models.ManyToManyField(to="Permission")
+    permissions = ManyToManyField(to="Permission")
 
     class Meta:
         db_table = "roles"
 
 
-class Permission(models.Model):
+class Permission(Model):
 
     """
     Model for role permissions entries.
@@ -83,19 +90,20 @@ class Permission(models.Model):
     This class models database table to store role permissions information.
 
     Attributes:
-        id (object): INTEGER column with auto increment used as permission id.
-        name (object): VARCHAR column to store name of permission.
-        created (object): DATETIME column to store date of permission creation.
-        updated (object): DATETIME column to store date of permission last updated.
-        description (object): VARCHAR column to store basic description of the
+        id (AutoField): INTEGER column with auto increment used as permission id.
+        name (CharField): VARCHAR column to store name of permission.
+        created (CharField): DATETIME column to store date of permission creation.
+        updated (DateTimeField): DATETIME column to store date of permission last
+            updated.
+        description (DateTimeField): VARCHAR column to store basic description of the
             permission.
     """
 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, null=False, unique=True)
-    description = models.CharField(max_length=150, null=True)
-    created = models.DateTimeField(default=now)
-    updated = models.DateTimeField(default=now)
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=50, null=False, unique=True)
+    description = CharField(max_length=150, null=True)
+    created = DateTimeField(default=now)
+    updated = DateTimeField(default=now)
 
     class Meta:
         db_table = "permissions"
