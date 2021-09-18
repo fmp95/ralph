16b23b0061b78abb338766108219666acc09bba7
Frederico Plotze <plotze.frederico.social@gmail.com>
2021-09-18 09:35:01 -0300

    Add user profile endpoint.

--- ralph/clients/authorization/router.py
+++ ralph/clients/authorization/router.py
@@ -1,6 +1,9 @@
+from typing import Dict
+
 from django.http import HttpRequest
 from ninja import Router
 
+from ralph.clients.authorization.auth import Authorization, AuthorizationResponse
 from ralph.clients.authorization.methods import login
 from ralph.clients.authorization.schemas import LoginSchema, TokenOut
 
@@ -16,6 +19,7 @@ def user_login(request: HttpRequest, data: LoginSchema) -> TokenOut:
     This endpoint is used to generate a Bearer token.
 
     Args:
+        request (HttpRequest): HTTP request.
         data (LoginSchema): Credentials for login.
 
     Returns:
@@ -23,3 +27,21 @@ def user_login(request: HttpRequest, data: LoginSchema) -> TokenOut:
     """
 
     return {"token": login(**data.dict())}
+
+
+@router.get("/user-profile", auth=Authorization())
+def user_profile(request: HttpRequest) -> Dict[str, AuthorizationResponse]:
+
+    """
+    Endpoint for retrieving user profile.
+
+    This endpoint is user to return all important user information to system.
+
+    Args:
+        request (HttpRequest): HTTP request.
+
+    Returns:
+        Dict[str, AuthorizationResponse]: Informations on user.
+    """
+
+    return {"data": request.auth}
